924e2ae4c967e1247a0ef70e8c94792a3aeadf56
Lukáš Jarmara <lukas.jarmara@gmail.com>
2025-07-29 15:50:48 +0200

    Two Crystal Balls

--- src/day1/TwoCrystalBalls.ts
+++ src/day1/TwoCrystalBalls.ts
@@ -1,12 +1,18 @@
 export default function two_crystal_balls(breaks: boolean[]): number {
-    let jumpAmount = Math.sqrt(breaks.length);
+    let jumpAmount = Math.floor(Math.sqrt(breaks.length));
 
-    let i = jumpAmount
-    for (; i < breaks.length; i += jumpAmount){
-        if (breaks[i]){
-            break
+    let i = jumpAmount;
+    for (; i < breaks.length; i += jumpAmount) {
+        if (breaks[i]) {
+            break;
         }
     }
 
-    if 
+    i -= jumpAmount;
+
+    for (let j = 0; j < jumpAmount && i < breaks.length; j++, i++) {
+        if (breaks[i]) {
+            return i;
+        }
+    }
 }
